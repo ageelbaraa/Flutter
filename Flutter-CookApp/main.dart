5b633555a63e8b51baf412d7443d5444c44272e7
ageelbaraa <ageelbaraa@gmail.com>
2021-10-05 14:01:24 +0300

    Update main.dart

--- Flutter-CookApp/main.dart
+++ Flutter-CookApp/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'WidgetsCatalog/TabBar/tab_view_for_ios.dart';
+import 'Navigation/simple_navigation_example.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,6 +13,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: CupertinoTabPage());
+        home: SimpleNavigationPage());
   }
 }
